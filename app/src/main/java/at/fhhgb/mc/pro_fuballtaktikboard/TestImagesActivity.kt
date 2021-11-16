cb869873e863b1541ea1afccebefa0a90ba8ea66
Fabian Hofer <s2010237027@fhooe.at>
2021-11-16 17:48:54 +0100

    added drawable icons and images

---
+++ app/src/main/java/at/fhhgb/mc/pro_fuballtaktikboard/TestImagesActivity.kt
@@ -0,0 +1,11 @@
+package at.fhhgb.mc.pro_fuballtaktikboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestImagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_images)
+    }
+}
